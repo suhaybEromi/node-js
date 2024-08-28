0b97f6ae9aec4d092b214c8f60a28d4fd4eb02fb
suhayb <suhayb@gmail.com>
2024-08-28 12:15:10 +0300

    Explain middleware and 3 paramiter (req,res,next)

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const http = require("http");
 const express = require("express");
-
 const app = express();
 
-const server = http.createServer(app);
+app.use((req, res, next) => {
+  console.log("In the middleware!");
+  next();
+});
 
+app.use((req, res, next) => {
+  console.log("In the another middleware!");
+  res.send("<h1>Welcome express-js</h1>");
+});
+
+const server = http.createServer(app);
 server.listen(3000);
