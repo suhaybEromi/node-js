c55bdb469927c5c0772549cc5da86053814b7bf5
suhayb <suhayb@gmail.com>
2024-08-29 18:21:00 +0300

    create views for used code html and create util for used path directory and used routes and mixing js and html

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 var bodyParser = require("body-parser");
 const app = express();
+const path = require("path");
 const adminRoutes = require("./routes/admin.js");
 const shopRoutes = require("./routes/shop.js");
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(adminRoutes);
-
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+
+app.use("/", (req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,10 @@
+const path = require("path");
 const express = require("express");
 const router = express.Router();
+const rootDir = require("../util/path");
 
 router.get("/", (req, res, next) => {
-  res.send("<h2>Hello from Express</h2>");
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
