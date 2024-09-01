f27745377386955601bb08e988920c994887045b
suhayb <suhayb@gmail.com>
2024-09-01 12:05:50 +0300

    used pug and dynamic value && create layouts and used little code with the help of layout

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,13 @@
 const path = require("path");
 const express = require("express");
 const router = express.Router();
+
 const rootDir = require("../util/path");
 const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
-  res.render("shop");
+  const products = adminData.products;
+  res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
