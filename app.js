cb79a67cef674b7e7b8915b06e7482ea01ef5318
suhayb <suhayb@gmail.com>
2024-09-02 18:36:31 +0300

    used ejs and dynamic value && used if statement and loop

--- app.js
+++ app.js
@@ -3,20 +3,9 @@ var bodyParser = require("body-parser");
 const path = require("path");
 const adminData = require("./routes/admin.js");
 const shopRoutes = require("./routes/shop.js");
-
-const expressHbs = require("express-handlebars");
 const app = express();
 
-app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-    extname: "hbs",
-  }),
-);
-
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
