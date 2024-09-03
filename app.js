ca703a97ec909e15ae2a8f95dd5bbcfb5e590c04
suhayb <suhayb@gmail.com>
2024-09-03 21:36:11 +0300

    start controller

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require("express");
 var bodyParser = require("body-parser");
 const path = require("path");
-const adminData = require("./routes/admin.js");
-const shopRoutes = require("./routes/shop.js");
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+const adminRoutes = require("./routes/admin.js");
+const shopRoutes = require("./routes/shop.js");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use("/", (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,20 +1,7 @@
-const path = require("path");
 const express = require("express");
 const router = express.Router();
+const productsController = require("../controllers/products");
 
-const rootDir = require("../util/path");
-const adminData = require("./admin");
-
-router.get("/", (req, res, next) => {
-  const products = adminData.products;
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
-  });
-});
+router.get("/", productsController.getProducts);
 
 module.exports = router;
