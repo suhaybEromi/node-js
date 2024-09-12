af20a99134156926c099766bca4e559ca71f88c3
suhayb <suhayb@gmail.com>
2024-09-12 11:08:11 +0300

    success delete functionallity

--- models/product.js
+++ models/product.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const utilPath = require("../util/path");
+const Cart = require("./cart");
 
 const p = path.join(utilPath, "data", "products.json");
 
@@ -41,6 +42,18 @@ module.exports = class Product {
           console.log(err);
         });
       }
+    });
+  }
+
+  static deleteById(id) {
+    getProductsFromFile(products => {
+      const product = products.find(prod => prod.id === id);
+      const updatedProducts = products.filter(prod => prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if (!err) {
+          Cart.deleteProduct(id, product.price);
+        }
+      });
     });
   }
 
--- routes/admin.js
+++ routes/admin.js
@@ -16,6 +16,10 @@ router.post("/add-product", adminController.postAddProduct);
 // /admin/edit-product => GET
 router.get("/edit-product/:productId", adminController.getEditProduct);
 
+// /admin/edit-product => POST
 router.post("/edit-product", adminController.postEditProduct);
+
+// /admin/delete-product => POST
+router.post("/delete-product", adminController.postDeleteProduct);
 
 module.exports = router;
