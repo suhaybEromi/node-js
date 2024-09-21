dc0ec8dd0acbfd32dfd9269a0489e478a17a4f6e
suhayb <suhayb@gmail.com>
2024-09-21 18:03:53 +0300

    create table user and relation with table

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    imageUrl: imageUrl,
-    price: price,
-    description: description,
-  })
+  req.user
+    .createProduct({
+      title: title,
+      imageUrl: imageUrl,
+      price: price,
+      description: description,
+    })
     .then(result => {
       // console.log(result);
       console.log("Created Product");
