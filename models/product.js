9605a2a1c7af6fa24fc6aaf92d0819fe790a8a8d
suhayb <suhayb@gmail.com>
2024-10-05 21:22:54 +0300

    update data and delete data with help mongodb

--- models/product.js
+++ models/product.js
@@ -2,18 +2,26 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection("products")
-      .insertOne(this)
+    let dbOp;
+    if (this._id) {
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: this._id }, { $set: this });
+      console.log("update", this._id);
+    } else {
+      dbOp = db.collection("products").insertOne(this);
+    }
+    return dbOp
       .then(result => {
         console.log(result);
       })
@@ -42,6 +50,17 @@ class Product {
       .then(product => {
         console.log(product);
         return product;
+      })
+      .catch(err => console.log(err));
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then(result => {
+        console.log(result);
       })
       .catch(err => console.log(err));
   }
