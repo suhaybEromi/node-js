82ffedb1d6f8707ebf37bac6d783b5ec49b042b3
suhayb <suhayb@gmail.com>
2024-10-22 17:20:47 +0300

    success functionality getCart and postCart and deleteCart

--- controllers/shop.js
+++ controllers/shop.js
@@ -44,8 +44,9 @@ exports.getIndex = (req, res, next) => {
 // Fetch and render the shopping cart
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
+    .populate("cart.items.productId")
+    .then(user => {
+      const products = user.cart.items;
       res.render("shop/cart", {
         path: "/cart",
         pageTitle: "Your Cart",
@@ -72,7 +73,7 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .deleteItemFromCart(prodId)
+    .removeFromCart(prodId)
     .then(result => {
       res.redirect("/cart");
     })
--- models/user.js
+++ models/user.js
@@ -19,11 +19,45 @@ const userSchema = new Schema({
           ref: "Product",
           required: true,
         },
-        qunatity: { type: Number, required: true },
+        quantity: { type: Number, required: true },
       },
     ],
   },
 });
+
+userSchema.methods.addToCart = function (product) {
+  // Check Id. NOTE
+  const cartProductIndex = this.cart.items.findIndex(cp => {
+    return cp.productId.toString() === product._id.toString();
+  });
+  // We retrieve All the data , Old And New. NOTE
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+  // if have data, you updated data. NOTE
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    // if do not have data, you add data. NOTE
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQuantity,
+    });
+  }
+  const updatedCart = {
+    items: updatedCartItems,
+  };
+  this.cart = updatedCart;
+  return this.save();
+};
+
+userSchema.methods.removeFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter(item => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart.items = updatedCartItems;
+  return this.save();
+};
 
 module.exports = mongoose.model("User", userSchema);
 
