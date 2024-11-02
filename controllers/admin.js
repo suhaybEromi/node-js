910d94b12887cf987e1f9941e9701b0c1a11d410
suhayb <suhayb@gmail.com>
2024-11-02 11:34:22 +0300

    create error message and use package connect-flash

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,7 +5,6 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     editing: false,
-    isAuthenticated: req.session.isLoggedIn,
   });
 };
 
@@ -46,7 +45,6 @@ exports.getEditProduct = (req, res, next) => {
         path: "/admin/edit-product",
         editing: editMode,
         product: product,
-        isAuthenticated: req.isLoggedIn,
       });
     })
     .catch(err => console.log(err));
@@ -109,7 +107,6 @@ exports.getProducts = (req, res, next) => {
         prods: products,
         pageTitle: "Admin Products",
         path: "/admin/products",
-        isAuthenticated: req.session.isLoggedIn,
       });
     })
     .catch(err => console.log(err));
