0edc18377dc2d518b794e96eef56df07b6775d6e
suhayb <suhayb@gmail.com>
2024-11-04 16:47:43 +0300

    start reset password

--- routes/auth.js
+++ routes/auth.js
@@ -12,4 +12,8 @@ router.post("/signup", authController.postSignup);
 
 router.post("/logout", authController.postLogout);
 
+router.get("/reset", authController.getReset);
+
+router.post("/reset", authController.postReset);
+
 module.exports = router;
