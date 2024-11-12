3c73e2e4125fa3c7f1546831b5ed57e0d4b90fbd
suhayb <suhayb@gmail.com>
2024-11-12 21:36:42 +0300

    add validation password and confirmPassword

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const authController = require("../controllers/auth");
 const router = express.Router();
-const { check } = require("express-validator");
+const { check, body } = require("express-validator");
 
 router.get("/login", authController.getLogin);
 
@@ -11,7 +11,29 @@ router.post("/login", authController.postLogin);
 
 router.post(
   "/signup",
-  check("email").isEmail().withMessage("Please enter a valid email"),
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .custom((value, { req }) => {
+        if (value === "test@test.com") {
+          throw new Error("This email address is forbidden.");
+        }
+        return true;
+      }),
+    body(
+      "password",
+      "Please enter a password with only numbers and text and at least 5 characters",
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Password have to match!");
+      }
+      return true;
+    }),
+  ],
   authController.postSignup,
 );
 
