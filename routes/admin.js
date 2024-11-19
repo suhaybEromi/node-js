30336539ac406db1c61c237ab79d3c23ee7206e1
suhayb <suhayb@gmail.com>
2024-11-19 21:54:00 +0300

    all validation code with the help of  express-validator

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const adminController = require("../controllers/admin");
 const isAuth = require("../middleware/is-auth");
+const { body } = require("express-validator");
 
 const router = express.Router();
 
@@ -12,12 +13,32 @@ router.get("/add-product", isAuth, adminController.getAddProduct);
 router.get("/products", isAuth, adminController.getProducts);
 
 // /admin/add-product => POST
-router.post("/add-product", isAuth, adminController.postAddProduct);
+router.post(
+  "/add-product",
+  [
+    body("title").isString().isLength({ min: 3 }).trim(),
+    body("imageUrl").isURL(),
+    body("price").isFloat(),
+    body("description").isLength({ min: 5, max: 400 }).trim(),
+  ],
+  isAuth,
+  adminController.postAddProduct,
+);
 
 // /admin/edit-product => GET
 router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
 
-router.post("/edit-product", isAuth, adminController.postEditProduct);
+router.post(
+  "/edit-product",
+  [
+    body("title").isString().isLength({ min: 3 }).trim(),
+    body("imageUrl").isURL(),
+    body("price").isFloat(),
+    body("description").isLength({ min: 5, max: 400 }).trim(),
+  ],
+  isAuth,
+  adminController.postEditProduct,
+);
 
 // /admin/delete-product => POST
 router.post("/delete-product", isAuth, adminController.postDeleteProduct);
