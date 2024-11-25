2b5c61ce9b2904433b6230613a2321a763d65616
suhayb <suhayb@gmail.com>
2024-11-25 22:32:56 +0300

    create folder and storage image and filter in image

--- app.js
+++ app.js
@@ -2,24 +2,46 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-require("dotenv").config();
-
-const errorController = require("./controllers/error");
-const User = require("./models/user");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const csrf = require("csurf");
 const flash = require("connect-flash");
+const multer = require("multer");
+require("dotenv").config();
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
 const app = express();
-
 const store = new MongoDBStore({
   uri: process.env.DATABASE_URL,
   collection: "sessions",
 });
-
 const csrfProtection = csrf();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      `${new Date().toISOString().replace(/:/g, "-")}-${file.originalname}`,
+    );
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -29,6 +51,13 @@ const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  multer({
+    dest: "images",
+    storage: fileStorage,
+    fileFilter: fileFilter,
+  }).single("image"),
+);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
@@ -38,7 +67,6 @@ app.use(
     store: store,
   }),
 );
-
 app.use(csrfProtection);
 app.use(flash());
 
@@ -49,7 +77,7 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  // throw new Error("Sync Dummy");
+  // throw new Error('Sync Dummy');
   if (!req.session.user) {
     return next();
   }
@@ -75,7 +103,8 @@ app.get("/500", errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  // res.redirect("/500");
+  // res.status(error.httpStatusCode).render(...);
+  // res.redirect('/500');
   res.status(500).render("500", {
     pageTitle: "Error!",
     path: "/500",
@@ -88,4 +117,6 @@ mongoose
   .then(result => {
     app.listen(3000);
   })
-  .catch(err => console.log(err));
+  .catch(err => {
+    console.log(err);
+  });
