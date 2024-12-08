022f08794fcba40177187b025ff4ee524bfdda5e
suhayb <suhayb@gmail.com>
2024-12-08 20:44:40 +0300

    finished async request

--- controllers/admin.js
+++ controllers/admin.js
@@ -201,8 +201,8 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then(product => {
       if (!product) {
@@ -213,11 +213,9 @@ exports.postDeleteProduct = (req, res, next) => {
     })
     .then(() => {
       console.log("Delete Product");
-      res.redirect("/admin/products");
+      res.status(200).json({ message: "Success" });
     })
     .catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({ message: "Deleting for products" });
     });
 };
